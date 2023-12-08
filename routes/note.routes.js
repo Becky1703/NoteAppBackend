b608fff87bf2db3f746ce938813ea4b247555495
Becky1703 <Becky1703>
2023-12-08 14:22:47 +0100

    added the route to allow user create a note

--- routes/note.routes.js
+++ routes/note.routes.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const { authenticator } = require("../middlewares/authenticator");
+const { NoteModel } = require("../models/NoteModel");
 
 const noteRouter = express.Router()
 noteRouter.use(authenticator)
 
+noteRouter.get("/", (req, res) => {
+    res.send({
+        message: ("All notes"),
+        status: 1
+    })
+
+})
+
+noteRouter.post("/create", async(req, res) => {
+    try {
+        let  note = new NoteModel(req.body)
+        await note.save()
+        res.send({
+            message: ("Note created"),
+            status: 1
+        })
+    } catch (error) {
+        res.send({
+            message: error.message,
+            status: 0
+        })
+    
+        
+    }
+})
 
 module.exports = {
     noteRouter,
