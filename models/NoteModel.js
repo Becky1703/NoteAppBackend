9fe3978fa2cbe244a9a72224141c617385d23167
Becky1703 <Becky1703>
2023-12-21 12:51:08 +0100

    added createdAt and updatedAt timestamp  and made changes to user route and Notemodel

--- models/NoteModel.js
+++ models/NoteModel.js
@@ -5,6 +5,9 @@ const noteSchema = new mongoose.Schema({
   title: { type: String, required: true },
   body: { type: String, required: true, unique: true },
   user: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  deletedAt: { type: Date, default: null }
 }, {
     versionKey: false
 })
