b45f02472f124438b1a1927f7d7d4c260d2d9cb6
Becky1703 <Becky1703>
2024-01-09 09:13:29 +0100

    removed commented out code

--- db.js
+++ db.js
@@ -6,18 +6,7 @@ const mongoose = require("mongoose")
 require("dotenv").config()
 
 // Connects to Mongodb database using the provided URL from the environment variable
-const connection =  mongoose.connect(process.env.mongoUrl);
-
-/** const connectDB = async () => {
-    try {
-      const conn = await mongoose.connect(process.env.mongoUrl);
-      console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  }*/
-  
+const connection =  mongoose.connect(process.env.mongoUrl);  
 
 // Exports the connection variable so it can be used in other files.
 module.exports = {
